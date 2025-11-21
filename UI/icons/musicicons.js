c39a92485816452890cda76a2210eaed1c1182a6
Soumya Goswami <24777304-SoumyaGoswami1@users.noreply.replit.com>
2025-11-21 07:39:16 +0000

    Initial commit

---
+++ UI/icons/musicicons.js
@@ -0,0 +1,20 @@
+const musicIcons = {
+    footerIcon: 'https://cdn.discordapp.com/emojis/865916418909536276.gif', 
+    correctIcon: 'https://cdn.discordapp.com/emojis/820971058089820200.gif', 
+    playerIcon: 'https://cdn.discordapp.com/emojis/992051186188435467.gif',
+    pauseresumeIcon:'https://cdn.discordapp.com/emojis/982935235807313971.gif',
+    loopIcon: 'https://cdn.discordapp.com/emojis/749272851529334795.gif',
+    beatsIcon: 'https://cdn.discordapp.com/emojis/928310693416009828.gif',
+    beats2Icon : 'https://cdn.discordapp.com/emojis/953413565400879165.gif',
+    alertIcon : 'https://cdn.discordapp.com/emojis/996431685358981201.gif',
+    skipIcon: 'https://cdn.discordapp.com/emojis/938388856095514654.gif',
+    stopIcon: 'https://cdn.discordapp.com/emojis/1021628438441902100.gif',
+    volumeIcon: 'https://cdn.discordapp.com/emojis/1040824501711159397.gif',
+    playlistIcon : 'https://cdn.discordapp.com/attachments/1393509889904607255/1441210794678157312/om-sign.gif?ex=6920f7a5&is=691fa625&hm=b911b719434946fb831cf0d755dd50a0c3e75e62e94c937f55462566375815e2&',
+    heartIcon : 'https://cdn.discordapp.com/attachments/1393509889904607255/1441210794678157312/om-sign.gif',
+    pingIcon : 'https://cdn.discordapp.com/emojis/923089856752664576.gif',
+    CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
+    MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
+};
+
+module.exports = musicIcons;
